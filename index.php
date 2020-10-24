1de44a8f5153f9cc2a7ce061e33938000b4a9b01
Tanvirul Haque <tanvirulhaque123@gmail.com>
2020-10-24 11:51:10 +0600

    Add Archive pagination

--- index.php
+++ index.php
@@ -23,36 +23,30 @@ get_header();
 			<div class="row">
 				<div class="column column-80 column-offset-10">
 					<?php
-					if ( have_posts() ) :
+					if ( have_posts() ) {
 
-						if ( is_home() && ! is_front_page() ) :
+						if ( is_home() && ! is_front_page() ) {
 							?>
 							<header>
 								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 							</header>
-						<?php
-						endif;
+							<?php
+						}
 
-						/* Start the Loop */
-						while ( have_posts() ) :
+						while ( have_posts() ) {
 							the_post();
 
-							/*
-							 * Include the Post-Type-specific template for the content.
-							 * If you want to override this in a child theme, then include a file
-							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-							 */
 							get_template_part( 'template-parts/content', get_post_type() );
 
-						endwhile;
+						}
 
-						the_posts_navigation();
+						the_posts_pagination();
 
-					else :
+					} else {
 
 						get_template_part( 'template-parts/content', 'none' );
 
-					endif;
+					}
 					?>
 				</div>
 			</div>
