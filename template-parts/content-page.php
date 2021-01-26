ca815e4ffb288143d898230d89f8559d073bc572
Tanvirul Haque <tanvirulhaque123@gmail.com>
2021-01-26 16:25:50 +0600

    Page title center

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="entry-header text-center">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
